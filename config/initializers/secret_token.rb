135987cfa16e8bf06d45024ca3df6b65f73588e2
pratik <pratik@joshsoftware.com>
2013-01-13 16:22:33 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puzzles::Application.config.secret_token = '1297a95a33f9e719ec0eacc124885628d51e5373db321e37950add6e5b5f6e23b5ab03ab027c22e15778652df577cf1672ae3bc5742eb934c0bd000e0848414a'
